1a4c9afc07e7cffcdb21c8f1cd35294fe7934cad
Elisa Situ Huang <elisasituhuang@gmail.com>
2022-11-22 10:31:12 -0500

    commit tree 5f1f4411b93e780d33beb6214ab2d520db061051

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name)
+    puts "Hello, #{name}!"
+end
+
+def add(a, b)
+    return a + b
+end
+
+def halve(a)
+   return a/2
+end
